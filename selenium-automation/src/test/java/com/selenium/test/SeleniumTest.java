687bbe03d95fe88a04e1fb81dfba9dc9676ee9d8
MilanJayasinghe <milansameera18@gmail.com>
2025-03-19 14:15:49 +0530

    first commit

---
+++ selenium-automation/src/test/java/com/selenium/test/SeleniumTest.java
@@ -0,0 +1,52 @@
+package com.selenium.test;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class SeleniumTest {
+
+    @Test
+    public void testGoogleSearch() {
+        System.setProperty("webdriver.chrome.driver", "D:\\chromedriver-win64\\chromedriver-win64\\chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        
+        driver.get("https://roopahala.com.au/");
+        System.out.println("Title: " + driver.getTitle());
+
+        driver.manage().window().maximize();
+
+        WebElement ModalClose = driver.findElement(By.xpath("//*[@id=\"newsletter-popup\"]/div/div/div/div/span/div"));
+		ModalClose.click();
+		
+		WebElement SignInbutton = driver.findElement(By.xpath("//*[@id=\"main-header\"]/div/div/div/div/nav/div[2]/ul/li[2]/a"));
+		SignInbutton.click();	
+		
+
+        // Switch to the alert
+        Alert alert = driver.switchTo().alert();
+        alert.accept();
+
+        WebElement emailfield = driver.findElement(By.xpath("//input[@name='email']"));
+		emailfield.sendKeys("milanjayasinghebc@gmail.com");
+		
+	//	WebElement nextbutton = driver.findElement(By.xpath("//button[@type='submit']"));
+		//nextbutton.click();
+		
+		WebElement namefield = driver.findElement(By.xpath("//input[@name='name']"));
+		WebElement passwordfield = driver.findElement(By.xpath("//input[@name='newPassword']"));
+		
+		namefield.sendKeys("Milan Jayasinghe");
+		passwordfield.sendKeys("123456");
+
+
+        driver.close();
+    }
+}
+
